546f09afbe12296c2f99f8308fe0e7428083079d
MarianoPedrotti <marianopedrotti43@gmail.com>
2026-03-17 16:41:23 -0300

    correccion de correo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav class="d-flex gap-2">
       <button class="btn btn-outline-primary btn-sm" @click="scrollTo('projects')">Proyectos</button>
       <button class="btn btn-outline-primary btn-sm" @click="scrollTo('contact')">Contacto</button>
-      <a href="/Mariano-Pedrotti-2026-BackendDeveloper.pdf" class="btn btn-primary btn-sm" download>Descargar CV</a>
+      <a href="/MarianoPedrotti-2026-BackendDeveloper.pdf" class="btn btn-primary btn-sm" download>Descargar CV</a>
     </nav>
   </div>
   
